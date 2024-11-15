ae5d3bfbff452f50d313fd36451b398de413cbba
earlaud <etienne.arlaud@inria.fr>
2024-11-15 15:39:59 +0100

    Trim whitespaces

--- scripts/subscriber_bullet.py
+++ scripts/subscriber_bullet.py
@@ -37,11 +37,11 @@ class MpcSubscriber(Node):
         super().__init__('mpc_subscriber')
         self.robotIf = Go2RobotInterface(self)
         self.start_mpc = False
-        
+
         # Define state publisher
         qos_profile = QoSProfile(depth=10)
         self.robot_pub = self.create_publisher(State, 'robot_states', qos_profile)
-        
+
         # Define command subscriber
         self.subscription = self.create_subscription(
             Torque,
@@ -61,16 +61,16 @@ class MpcSubscriber(Node):
         # Define at which rate the simulation state is sent to rviz
         timer_period = 0.001  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        
+
         # Message declarations for torque
         self.torque_simu = np.zeros(18)
         self.current_torque = np.zeros(12)
 
-        # Load the robot model 
+        # Load the robot model
         self.rmodel, geom_model = loadGo2()
 
         self.space = manifolds.MultibodyPhaseSpace(self.rmodel)
-        
+
         self.q_current = np.array([0, 0, 0.335, 0, 0, 0, 1,
             0.068, 0.785, -1.440,
             -0.068, 0.785, -1.440,
@@ -83,17 +83,17 @@ class MpcSubscriber(Node):
         self.nu = 12
         self.nq = 19
         self.x0 = np.concatenate((self.q_current, self.v_current))
-        self.u0 = np.array([-3.71, -1.81,  5.25,  
-                            3.14, -1.37, 5.54, 
-                            -1.39, -1.09,  3.36,  
+        self.u0 = np.array([-3.71, -1.81,  5.25,
+                            3.14, -1.37, 5.54,
+                            -1.39, -1.09,  3.36,
                             1.95, -0.61,  3.61])
         self.K0 = np.zeros((12, 36))
-        
+
         # Define default PD controller that runs before MPC launch
         gain = 100
         self.Kp = np.ones(12) * gain
         self.Kd = np.ones(12) * 1
-            
+
 
     def listener_callback(self, msg):
         self.u0 = np.array(msg.u0.tolist())
@@ -101,7 +101,7 @@ class MpcSubscriber(Node):
         self.K0 = np.array(msg.riccati.tolist()).reshape((self.nu, self.ndx))
 
         self.start_mpc = True
-    
+
     def listener_callback_odom(self, msg):
         self.q_current[0] = msg.pose.pose.position.x
         self.q_current[1] = msg.pose.pose.position.y
@@ -117,7 +117,7 @@ class MpcSubscriber(Node):
         self.v_current[3] = msg.twist.twist.angular.x
         self.v_current[4] = msg.twist.twist.angular.y
         self.v_current[5] = msg.twist.twist.angular.z
-    
+
     def timer_callback(self):
         current_tqva = self.robotIf.get_joint_state()
         if current_tqva is None:
@@ -125,20 +125,20 @@ class MpcSubscriber(Node):
 
         self.q_current[7:] = current_tqva[1]
         self.v_current[6:] = current_tqva[2]
-        
+
         if not(self.start_mpc):
             self.current_torque = self.u0 - self.Kp @ (self.q_current[7:] - self.x0[7:self.nq]) - self.Kd @ self.v_current[6:]
         else:
             x_measured = np.concatenate((self.q_current, self.v_current))
             self.current_torque = self.u0 - self.K0 @ self.space.difference(x_measured, self.x0)
-        
+
         if (self.robotIf.is_init):
-            self.robotIf.send_command(self.q_current[7:].tolist(), 
-                                    self.v_current[6:].tolist(), 
-                                    self.current_torque.tolist(), 
-                                    self.Kp.tolist(), 
+            self.robotIf.send_command(self.q_current[7:].tolist(),
+                                    self.v_current[6:].tolist(),
+                                    self.current_torque.tolist(),
+                                    self.Kp.tolist(),
                                     self.Kd.tolist())
-        
+
             state = State()
             state.qc = self.q_current
             state.vc = self.v_current
@@ -151,7 +151,7 @@ def main(args=None):
 
     thread = threading.Thread(target=rclpy.spin, args=(mpc_subscriber, ), daemon=True)
     thread.start()
-    
+
     mpc_subscriber.robotIf.start(mpc_subscriber.q_current[7:].tolist())
     input("Ready to start...")
 
