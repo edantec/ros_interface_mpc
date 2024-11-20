79f69fec5fb78d0ddb56aebeaadc1c77ee398186
Ewen Dantec <ewen.dantec@inria.fr>
2024-11-20 09:56:39 +0100

    Comment mpc.py

--- scripts/mpc.py
+++ scripts/mpc.py
@@ -6,181 +6,7 @@ The contacts forces are modeled as 6D wrenches.
 
 import numpy as np
 import example_robot_data
-from simple_mpc import RobotHandler, FullDynamicsProblem, KinodynamicsProblem, CentroidalProblem, MPC
-
-class TalosParameters():
-    def __init__(self, mpc_type):
-        print(mpc_type)
-        URDF_FILENAME = "talos_reduced.urdf"
-        SRDF_FILENAME = "talos.srdf"
-        SRDF_SUBPATH = "/talos_data/srdf/" + SRDF_FILENAME
-        URDF_SUBPATH = "/talos_data/robots/" + URDF_FILENAME
-
-        modelPath = example_robot_data.getModelPath(URDF_SUBPATH)
-        
-        design_conf = dict(
-            urdf_path=modelPath + URDF_SUBPATH,
-            srdf_path=modelPath + SRDF_SUBPATH,
-            robot_description="",
-            root_name="root_joint",
-            base_configuration="half_sitting",
-            controlled_joints_names=[
-                "root_joint",
-                "leg_left_1_joint",
-                "leg_left_2_joint",
-                "leg_left_3_joint",
-                "leg_left_4_joint",
-                "leg_left_5_joint",
-                "leg_left_6_joint",
-                "leg_right_1_joint",
-                "leg_right_2_joint",
-                "leg_right_3_joint",
-                "leg_right_4_joint",
-                "leg_right_5_joint",
-                "leg_right_6_joint",
-                "torso_1_joint",
-                "torso_2_joint",
-                "arm_left_1_joint",
-                "arm_left_2_joint",
-                "arm_left_3_joint",
-                "arm_left_4_joint",
-                "arm_right_1_joint",
-                "arm_right_2_joint",
-                "arm_right_3_joint",
-                "arm_right_4_joint",
-            ],
-            end_effector_names=[
-                "left_sole_link",
-                "right_sole_link",
-            ],
-        )
-        self.handler = RobotHandler()
-        self.handler.initialize(design_conf)
-        gravity = np.array([0, 0, -9.81])
-        u0_forces = np.zeros(self.handler.getModel().nv + 6)
-        for i in range(2):
-            u0_forces[6 * i + 2] = -gravity[2] * self.handler.getMass() / 2
-
-        if (mpc_type == "fulldynamics"):
-            w_x_vec = np.array(
-                [
-                    0, 0, 0, 100, 100, 100,  # Base pos/ori
-                    0.1, 0.1, 0.1, 0.1, 0.1, 0.1,  # Left leg
-                    0.1, 0.1, 0.1, 0.1, 0.1, 0.1,  # Right leg
-                    10, 10,  # Torso
-                    1, 1, 1, 1,  # Left arm
-                    1, 1, 1, 1,  # Right arm
-                    1, 1, 1, 1, 1, 1,  # Base pos/ori vel
-                    0.1, 0.1, 0.1, 0.1, 1, 1,  # Left leg vel
-                    0.1, 0.1, 0.1, 0.1, 1, 1,  # Right leg vel
-                    10, 10,  # Torso vel
-                    1, 1, 1, 1,  # Left arm vel
-                    1, 1, 1, 1,  # Right arm vel
-                ]
-            )
-            w_cent_lin = np.array([0.0, 0.0, 10.])
-            w_cent_ang = np.array([0., 0., 10])
-            w_forces_lin = np.ones(3) * 0.0001
-            w_forces_ang = np.ones(3) * 0.0001
-
-            nu = self.handler.getModel().nv - 6
-        
-            self.problem_conf = dict(
-                x0=self.handler.getState(),
-                u0=np.zeros(nu),
-                DT=0.01,
-                w_x=np.diag(w_x_vec),
-                w_u=np.eye(nu) * 1e-4,
-                w_cent=np.diag(np.concatenate((w_cent_lin, w_cent_ang))),
-                gravity=gravity,
-                force_size=6,
-                w_forces=np.diag(np.concatenate((w_forces_lin, w_forces_ang))),
-                w_frame=np.eye(6) * 4000,
-                umin=-self.handler.getModel().effortLimit[6:],
-                umax=self.handler.getModel().effortLimit[6:],
-                qmin=self.handler.getModel().lowerPositionLimit[7:],
-                qmax=self.handler.getModel().upperPositionLimit[7:],
-                mu=0.8,
-                Lfoot=0.1,
-                Wfoot=0.075,
-            )
-        elif (mpc_type == "kinodynamics"):
-            w_x = np.array([
-                0, 0, 1000, 1000, 1000, 1000, # Base pos/ori
-                0.1, 0.1, 0.1, 0.1, 0.1, 0.1, # Left leg
-                0.1, 0.1, 0.1, 0.1, 0.1, 0.1, # Right leg
-                1, 1000, # Torso
-                1, 1, 10, 10, # Left arm
-                1, 1, 10, 10, # Right arm
-                0.1, 0.1, 0.1, 1000, 1000, 1000, # Base pos/ori vel
-                1, 1, 1, 1, 1, 1, # Left leg vel
-                1, 1, 1, 1, 1, 1, # Right leg vel
-                0.1, 100, # Torso vel
-                10, 10, 10, 10, # Left arm vel
-                10, 10, 10, 10, # Right arm vel
-            ]) 
-            w_x = np.diag(w_x) * 10
-            w_linforce = np.array([0.001,0.001,0.01])
-            w_angforce = np.ones(3) * 0.1
-            w_u = np.concatenate((
-                w_linforce, 
-                w_angforce,
-                w_linforce, 
-                w_angforce,
-                np.ones(self.handler.getModel().nv - 6) * 1e-4
-            ))
-            w_u = np.diag(w_u) 
-            w_LFRF = 100000
-            w_cent_lin = np.array([0.0,0.0,1])
-            w_cent_ang = np.array([0.1,0.1,10])
-            w_cent = np.diag(np.concatenate((w_cent_lin,w_cent_ang)))
-            w_centder_lin = np.ones(3) * 0.
-            w_centder_ang = np.ones(3) * 0.1
-            w_centder = np.diag(np.concatenate((w_centder_lin,w_centder_ang)))
-
-            self.problem_conf = dict(
-                x0=self.handler.getState(),
-                u0=u0_forces,
-                DT=0.01,
-                w_x=w_x,
-                w_u=w_u,
-                w_cent=w_cent,
-                w_centder=w_centder,
-                gravity=gravity,
-                force_size=6,
-                w_frame=np.eye(6) * w_LFRF,
-                umin=-self.handler.getModel().effortLimit[6:],
-                umax=self.handler.getModel().effortLimit[6:],
-                qmin=self.handler.getModel().lowerPositionLimit[7:],
-                qmax=self.handler.getModel().upperPositionLimit[7:],
-            )
-        elif (mpc_type == "centroidal"):
-            w_control_linear = np.ones(3) * 0.001
-            w_control_angular = np.ones(3) * 0.1
-            w_control = np.diag(np.concatenate((
-                w_control_linear,
-                w_control_angular,
-                w_control_linear,
-                w_control_angular
-            )))
-
-            self.problem_conf = dict(
-                x0=self.handler.getState(),
-                u0=u0_forces,
-                DT=0.01,
-                w_x=w_x,
-                w_u=w_control,
-                w_centroidal_com=np.diag(np.array([0,0,0])),
-                w_linear_mom=np.diag(np.array([0.01,0.01,100])),
-                w_linear_acc=0.01 * np.eye(3),
-                w_angular_mom=np.diag(np.array([0.1,0.1,1000])),
-                w_angular_acc=0.01 * np.eye(3),
-                gravity=gravity,
-                force_size=6,
-            )
-        else:
-            print("Error: MPC type not recognized")
-            exit()
+from simple_mpc import RobotHandler, FullDynamicsProblem, KinodynamicsProblem, MPC
 
 class Go2Parameters():
     def __init__(self, mpc_type):
@@ -232,27 +58,43 @@ class Go2Parameters():
             u0_forces[6 * i + 2] = -gravity[2] * self.handler.getMass() / 2
 
         if (mpc_type == "fulldynamics"):
+            # Weight for base position and orientation
             w_basepos = [0, 0, 1, 0, 0, 0]
-            w_legpos = [0.1, 0.1, 0.1]
 
+            # Weight for leg position (hip, thigh, ankle)
+            w_legpos = [0.1, 0.1, 0.1]
+           
+            # Weight for base linear and angular velocity
             w_basevel = [10, 10, 10, 10, 10, 10]
+
+            # Weight for leg velocity (hip, thigh, ankle)
             w_legvel = [1, 1, 1]
+
+            # Concatenated state weight
             w_x = np.array(w_basepos + w_legpos * 4 + w_basevel + w_legvel * 4)
+
+            # Weight for linear momentum regularization
             w_cent_lin = np.array([1, 1, 1])
+            # Weight for angular momentum regularization
             w_cent_ang = np.array([0.1, 0.1, 1])
+
+            # Weight for force regularization (reference is robot weight divided by nb of contacts)
             w_forces_lin = np.array([0.0002, 0.0002, 0.0002])
+            
+            # Weight for feet position tracking
+            w_foot_tracking = 5000
 
             nu = self.handler.getModel().nv - 6
         
             self.problem_conf = dict(
                 DT=0.01,
                 w_x=np.diag(w_x),
-                w_u=np.eye(nu) * 1e-4,
+                w_u=np.eye(nu) * 1e-4, # Weight for torque regularization
                 w_cent=np.diag(np.concatenate((w_cent_lin, w_cent_ang))),
                 gravity=gravity,
                 force_size=3,
                 w_forces=np.diag(w_forces_lin),
-                w_frame=np.eye(3) * 5000,
+                w_frame=np.eye(3) * w_foot_tracking,
                 umin=-self.handler.getModel().effortLimit[6:],
                 umax=self.handler.getModel().effortLimit[6:],
                 qmin=self.handler.getModel().lowerPositionLimit[7:],
@@ -265,30 +107,54 @@ class Go2Parameters():
                 force_cone=False,
             )
         elif (mpc_type == "kinodynamics"):
+            # Weight for base position and orientation
             w_basepos = [0, 0, 0, 0, 0, 0]
-            w_legpos = [1, 1, 1]
 
+             # Weight for leg position (hip, thigh, ankle)
+            w_legpos = [1, 1, 1]
+            
+            # Weight for base linear and angular velocity
             w_basevel = [10, 10, 10, 10, 10, 10]
+
+            # Weight for leg velocity (hip, thigh, ankle)
             w_legvel = [0.1, 0.1, 0.1]
-            w_x = np.array(w_basepos + w_legpos * 4 + w_basevel + w_legvel * 4)
-            w_x = np.diag(w_x)
-            w_linforce = np.array([0.001, 0.001, 0.001])
-            w_u = np.concatenate(
+            
+            # Concatenated weight for state regularization
+            w_x = np.diag(np.array(w_basepos + w_legpos * 4 + w_basevel + w_legvel * 4))
+
+            # Weight for force regularization (reference is robot weight divided by nb of contacts)
+            w_forces_lin = np.array([0.001, 0.001, 0.001])
+
+            # Concatenated weight for force and acceleration regularization
+            w_u = np.diag(np.concatenate(
                 (
-                    w_linforce,
-                    w_linforce,
-                    w_linforce,
-                    w_linforce,
+                    w_forces_lin,
+                    w_forces_lin,
+                    w_forces_lin,
+                    w_forces_lin,
                     np.ones(self.handler.getModel().nv - 6) * 1e-4,
                 )
-            )
-            w_u = np.diag(w_u)
-            w_LFRF = 3000
+            ))
+            
+            # Weight for feet position tracking
+            w_foot_tracking = 3000
+
+            # Weight for linear momentum regularization
             w_cent_lin = np.array([0.1, 0.1, 1])
+
+            # Weight for angular momentum regularization
             w_cent_ang = np.array([0.1, 0.1, 1])
+
+            # Concatenated weight for momentum reg
             w_cent = np.diag(np.concatenate((w_cent_lin, w_cent_ang)))
+
+            # Weight for derivative of linear momentum regularization
             w_centder_lin = np.ones(3) * 0.0
+
+            # Weight for derivative of angular momentum regularization
             w_centder_ang = np.ones(3) * 0.1
+
+            # Concatenated weight for derivative of momentum reg
             w_centder = np.diag(np.concatenate((w_centder_lin, w_centder_ang)))
 
             self.problem_conf = dict(
@@ -299,7 +165,7 @@ class Go2Parameters():
                 w_centder=w_centder,
                 gravity=gravity,
                 force_size=3,
-                w_frame=np.eye(3) * w_LFRF,
+                w_frame=np.eye(3) * w_foot_tracking,
                 umin=-self.handler.getModel().effortLimit[6:],
                 umax=self.handler.getModel().effortLimit[6:],
                 qmin=self.handler.getModel().lowerPositionLimit[7:],
@@ -308,7 +174,7 @@ class Go2Parameters():
                 Lfoot=0.01,
                 Wfoot=0.01,
                 kinematics_limits=False,
-                force_cone=True,
+                force_cone=False,
             )
         else:
             print("Error: MPC type not recognized")
@@ -393,58 +259,4 @@ class ControlBlockGo2():
         self.mpc.generateCycleHorizon(contact_phases)
 
     def update_mpc(self, x_measured):
-        self.mpc.iterate(x_measured[:self.nq],x_measured[self.nq:])
-
-class ControlBlock():
-    def __init__(self, mpc_type):
-        print(mpc_type)
-        self.param = TalosParameters(mpc_type)
-        self.T = 100
-
-        problem = FullDynamicsProblem(self.param.handler)
-        problem.initialize(self.param.problem_conf)
-        problem.createProblem(self.param.problem_conf["x0"], self.T, 6, self.param.problem_conf["gravity"][2])
-
-        mpc_conf = dict(
-            ddpIteration=1,
-            support_force=-self.param.handler.getMass() * self.param.problem_conf["gravity"][2],
-            TOL=1e-4,
-            mu_init=1e-8,
-            max_iters=1,
-            num_threads=8,
-        )
-
-        self.mpc = MPC()
-        self.mpc.initialize(mpc_conf, problem)
-        self.nq = self.param.handler.getModel().nq
-    
-    def create_gait(self):
-        """ Define gait and time parameters"""
-        T_ds = 20
-        T_ss = 80
-        dt = 0.01
-        self.Nsimu = int(dt / 0.001)
-
-        """ Define contact sequence throughout horizon"""
-        contact_phase_double = {
-            "left_sole_link" : True,
-            "right_sole_link" : True,
-        }
-        contact_phase_left = {
-            "left_sole_link" : True,
-            "right_sole_link" : False,
-        }
-        contact_phase_right = {
-            "left_sole_link" : False,
-            "right_sole_link" : True,
-        }
-        contact_phases = [contact_phase_double] * T_ds
-        contact_phases += [contact_phase_left] * T_ss
-        contact_phases += [contact_phase_double] * T_ds
-        contact_phases += [contact_phase_right] * T_ss 
-
-        self.mpc.generateCycleHorizon(contact_phases)
-
-    def update_mpc(self, x_measured):
-
         self.mpc.iterate(x_measured[:self.nq],x_measured[self.nq:])
